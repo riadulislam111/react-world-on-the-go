172d20f682304ae1744c2c1cdc89b88af9ef9dc3
Jarif Ahmed <your_github_email@gmail.com>
2026-03-02 16:06:57 +0600

    added all file updates

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,12 +1,21 @@
 import React, { use } from 'react';
+import countrys from '../Country/country';
+import './Countries.css';
+
+
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
-    console.log(countries);
+    // console.log(countries);
     return (
         <div>
             <h1>In the countries: {countries.length}</h1>
+            <div className='countries'>
+            {
+                countries.map(country => <Countries key={country.cca3.cca3} country= {country}></Countries>)
+            }
+        </div>
         </div>
     );
 };
--- src/components/Country/country.jsx
+++ src/components/Country/country.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './country.css';
 
-const country = () => {
+const country = ({country}) => {
+// console.log(country.name.common)
+
+const handleVisited = () => {
+    console.log('button clicked')
+}
+
+
+
     return (
-        <div>
-            <h3>Name:</h3>
+        <div className='country'>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name:{country.name.common}</h3>
+            <p>population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : 'Small Country'}</p>
+            <button onClick={handleVisited}>Not Visited</button>
         </div>
     );
 };
